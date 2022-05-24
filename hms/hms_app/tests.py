bb5636d87025eff7149c3b9623d148b3a996a700
Philip Thapa <philipthapa3@gmail.com>
2022-05-24 15:25:23 +0530

    Added normal testcases

--- hms/hms_app/tests.py
+++ hms/hms_app/tests.py
@@ -1,41 +1,53 @@
-from rest_framework.test import APITestCase
-from rest_framework.authtoken.models import Token
-from rest_framework import status
-# from django.contrib.auth.models import User
-from django.urls import reverse
+# from rest_framework.test import APITestCase
+# from rest_framework.authtoken.models import Token
+# from rest_framework import status
+# from django.urls import reverse
+#
+# from .models import User
+#
+#
+# class RegisterTestCase(APITestCase):
+#
+#     def test_register(self):
+#         data = {
+#             "email": "Jenkins1@hms.com",
+#             "password": "12345"
+#         }
+#         response = self.client.post(reverse('ek/register_user_name'), data)
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#
+#
+# class LoginLogoutTestCase(APITestCase):
+#
+#     def setUp(self):
+#         self.user = User.objects.create_user(email="Jenkins1@hms.com",
+#                                                    password="12345")
+#
+#     def test_login(self):
+#         data = {
+#             "email": "Jenkins1@hms.com",
+#             "password": "12345"
+#         }
+#         response = self.client.post(reverse('ek/login'), data)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
+#     def test_logout(self):
+#         self.token = Token.objects.get(user__email="Jenkins1@hms.com")
+#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+#         response = self.client.post(reverse('ek/logout'))
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+from django.test import TestCase
 
-
-# Create your tests here.
-from .models import User
-
-
-class RegisterTestCase(APITestCase):
-
-    def test_register(self):
-        data = {
-            "email": "Jenkins1@hms.com",
-            "password": "12345"
-        }
-        response = self.client.post(reverse('ek/register_user_name'), data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-
-class LoginLogoutTestCase(APITestCase):
-
+class YourTestClass(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(email="Jenkins1@hms.com",
-                                                   password="12345")
+        pass
 
-    def test_login(self):
-        data = {
-            "email": "Jenkins1@hms.com",
-            "password": "12345"
-        }
-        response = self.client.post(reverse('ek/login'), data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
 
-    def test_logout(self):
-        self.token = Token.objects.get(user__email="Jenkins1@hms.com")
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        response = self.client.post(reverse('ek/logout'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_something_that_will_pass(self):
+        self.assertTrue(4 == 2+2)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
