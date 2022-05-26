ea9203c67447dd593f2d7b19352283b30c3b6bd0
Philip Thapa <philipthapa3@gmail.com>
2022-05-26 12:06:53 +0530

    Configured web hooks

--- hms/hms_app/tests.py
+++ hms/hms_app/tests.py
@@ -1,41 +1,47 @@
+# # from django.test import TestCase
 # from rest_framework.test import APITestCase
 # from rest_framework.authtoken.models import Token
 # from rest_framework import status
+# # from django.contrib.auth.models import User
 # from django.urls import reverse
 #
+# import os
 # from .models import User
+# from django.core.files.uploadedfile import SimpleUploadedFile
 #
 #
-# class RegisterTestCase(APITestCase):
+# class RegisterTests(APITestCase):
 #
 #     def test_register(self):
+#         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #         data = {
-#             "email": "Jenkins1@hms.com",
-#             "password": "12345"
+#             "email": "example@gmail.com",
+#             "password": "Test123",
 #         }
 #         response = self.client.post(reverse('register_user_name'), data)
 #         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 #
 #
-# class LoginLogoutTestCase(APITestCase):
+# class LoginLogoutTests(APITestCase):
 #
 #     def setUp(self):
-#         self.user = User.objects.create_user(email="Jenkins1@hms.com",
-#                                                    password="12345")
+#         self.user = User.objects.create_user(email="example@gmail.com",
+#                                                    password="Test123")
 #
 #     def test_login(self):
 #         data = {
-#             "email": "test1@hms.com",
-#             "password": "12345"
+#             "email": "example@gmail.com",
+#             "password": "Test123",
 #         }
 #         response = self.client.post(reverse('signin'), data)
 #         self.assertEqual(response.status_code, status.HTTP_200_OK)
 #
 #     def test_logout(self):
-#         self.token = Token.objects.get(user__email="test1@hms.com")
+#         self.token = Token.objects.get(user=self.user)
 #         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 #         response = self.client.post(reverse('signout'))
 #         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
 
 from django.test import TestCase
@@ -59,3 +65,5 @@ class YourTestClass(TestCase):
 
     def test_total_sum(self):
         self.assertTrue(4 == 2+2)
+    def total_cal(self):
+        self.assertEqual(4 == 2*2)
