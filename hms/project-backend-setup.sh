baeb57c22a73e499235c3c3bfced07abaf29e7a8
Philip Thapa <68701849+philip-thapa@users.noreply.github.com>
2022-06-06 18:33:42 +0530

    Update project-backend-setup.sh

--- hms/project-backend-setup.sh
+++ hms/project-backend-setup.sh
@@ -1,1 +1,22 @@
-
+sudo apt update
+sudo apt-get install wget -y
+wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
+sudo apt-get install build-essential checkinstall -y
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
+sudo apt-get install zlib1g-dev -y
+tar -xzvf Python-3.6.8.tgz
+cd Python-3.6.8
+./configure --enable-optimizations
+sudo make altinstall
+sudo apt-get install libmysqlclient-dev -y
+sudo apt-get install libldap2-dev -y
+sudo apt-get install libsasl2-dev -y
+sudo apt-get install gcc libpq-dev -y
+sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+sudo apt-get install -y locales locales-all
+cd
+mkdir venv
+python3.6 -m venv venv/venv-medplus
+#source venv/venv-medplus/bin/activate
+venv/venv-medplus/bin/pip install wheel
+venv/venv-medplus/bin/pip install -r requirements.txt
