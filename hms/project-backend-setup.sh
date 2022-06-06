6ae8c0656625c0a6f081b26942d06ca2f81036d7
Philip Thapa <68701849+philip-thapa@users.noreply.github.com>
2022-06-06 18:43:14 +0530

    Update project-backend-setup.sh

--- hms/project-backend-setup.sh
+++ hms/project-backend-setup.sh
@@ -16,7 +16,8 @@ sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
 sudo apt-get install -y locales locales-all
 cd
 mkdir venv
+pwd
 python3.6 -m venv venv/venv-medplus
 #source venv/venv-medplus/bin/activate
 venv/venv-medplus/bin/pip install wheel
-venv/venv-medplus/bin/pip install -r requirements.txt
+venv/venv-medplus/bin/pip install -r ../requirements.txt
